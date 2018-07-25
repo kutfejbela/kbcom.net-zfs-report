1bd0f745e40bf1d4b49c9798d7cbe20ad53884f3
root <root@szerver05.petav.local>
2018-07-25 12:09:06 +0200

    Install script

--- install.bash
+++ install.bash
@@ -10,19 +10,19 @@ CONFIG_FOLDER_MAIN=\"$(/bin/pwd)\"
 source \"\$CONFIG_FOLDER_MAIN/etc/kbcom.net-zfs-report.conf\"
 
 \"\$CONFIG_FOLDER_MAIN/usr/share/kbcom.net-zfs-report.bash\"
-" > "run.bash"
+" 1>"run.bash"
 
 /bin/chmod a+x "run.bash"
 
 #echo "# ZFS ARCSTATS log file
 #export CONFIG_FILE_ARCSTATSLOG=\"$CONFIG_FOLDER_MAIN/var/kbcom.net-zfs-report.log\"
-#" > etc/kbcom.net-zfs-report.conf
+#" 1> "etc/kbcom.net-zfs-report.conf"
 
 
 echo "MAILTO=\"$MAILTO\"
 MAILFROM=\"$MAILFROM\"
 
 0 23 * * *  root  $(/bin/pwd)/run.bash
-" > "local/etc/cron.d/kbcom.net-zfs-report"
+" 1>"local/etc/cron.d/kbcom.net-zfs-report"
 
 /bin/ln -i -s "$(/bin/pwd)/local/etc/cron.d/kbcom.net-zfs-report" "/etc/cron.d/zfs-report"
