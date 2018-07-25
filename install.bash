321274b1de9672012bd7ce26ebf6e0dc82f5bfd1
root <root@szerver05.petav.local>
2018-07-25 11:28:44 +0200

    Delta

--- install.bash
+++ install.bash
@@ -3,9 +3,21 @@
 read -e -p "Enter cron E-Mail to: " -i "zfs@mail.$(/bin/domainname)" MAILTO
 read -e -p "Enter cron E-Mail from: " -i "cron@$(/bin/hostname -f)" MAILFROM
 
+echo "#!/bin/bash
+
+CONFIG_FOLDER_MAIN=\"$(/bin/pwd)\"
+
+source \"$CONFIG_FOLDER_MAIN/etc/kbcom.net-zfs-report.conf\"
+
+\"$(/bin/pwd)/usr/share/kbnet.com-zfs-report.bash\"
+" > "$(/bin/pwd)/run.bash"
+
+/bin/chmod a+x "$(/bin/pwd)/run.bash"
+
 echo "MAILTO=\"$MAILTO\"
 MAILFROM=\"$MAILFROM\"
 
-0 23 * * *  root  $(/bin/pwd)/usr/share/kbnet.com-zfs-report.bash" > local/etc/cron.d/kbnet.com-zfs-report
+0 23 * * *  root  $(/bin/pwd)/run.bash"
+> "local/etc/cron.d/kbnet.com-zfs-report"
 
-/bin/ln -i -s "$(/bin/pwd)/local/etc/cron.d/kbnet.com-zfs-report" /etc/cron.d/zfs-report
+/bin/ln -i -s "$(/bin/pwd)/local/etc/cron.d/kbnet.com-zfs-report" "/etc/cron.d/zfs-report"
---
+++ usr/share/kbcom.net-zfs-report.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CONFIG_FOLDER_MAIN="/opt/.github/kbcom.net-zfs-report"
+CONFIG_FILE_ARCSTATSLOG="$CONFIG_FOLDER_MAIN/var/kbcom.net-zfs-report.log"
+
+GLOBAL_FOLDER_SCRIPT=$(/usr/bin/dirname "$0")
+source "$GLOBAL_FOLDER_SCRIPT/.kbcom.net-zfs-report.bash"
+
+zpool_print_status
+echo
+zpool_print_verbosediostat
+
+
+IFS=$'\n'
+GLOBAL_DATETIME="$(/bin/date)"
+zfs_getarray_arcstats
+
+
+if [ -f "$CONFIG_FILE_ARCSTATSLOG" ]
+then
+ echo "Last log date: $(zfs_getdate_arcstatslog "$CONFIG_FILE_ARCSTATSLOG")"
+ zfs_getarray_arcstatslog "$CONFIG_FILE_ARCSTATSLOG"
+
+ zfs_write_arcstatslog "$GLOBAL_DATETIME" "$CONFIG_FILE_ARCSTATSLOG"
+
+ zfs_print_arcstatslog
+else
+ zfs_write_arcstatslog "$GLOBAL_DATETIME" "$CONFIG_FILE_ARCSTATSLOG"
+
+ zfs_print_arcstats
+fi
