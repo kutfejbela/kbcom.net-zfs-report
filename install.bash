4faf8367c0862bff175dc7396a1a20317228a7c1
root <root@szerver05.petav.local>
2018-07-25 11:40:20 +0200

    Install script

--- install.bash
+++ install.bash
@@ -7,17 +7,17 @@ echo "#!/bin/bash
 
 CONFIG_FOLDER_MAIN=\"$(/bin/pwd)\"
 
-source \"$CONFIG_FOLDER_MAIN/etc/kbcom.net-zfs-report.conf\"
+source \"\$CONFIG_FOLDER_MAIN/etc/kbcom.net-zfs-report.conf\"
 
-\"$(/bin/pwd)/usr/share/kbnet.com-zfs-report.bash\"
-" > "$(/bin/pwd)/run.bash"
+\"\$CONFIG_FOLDER_MAIN/usr/share/kbcom.net-zfs-report.bash\"
+" > "run.bash"
 
-/bin/chmod a+x "$(/bin/pwd)/run.bash"
+/bin/chmod a+x "run.bash"
 
 echo "MAILTO=\"$MAILTO\"
 MAILFROM=\"$MAILFROM\"
 
 0 23 * * *  root  $(/bin/pwd)/run.bash"
-> "local/etc/cron.d/kbnet.com-zfs-report"
+> "local/etc/cron.d/kbcom.net-zfs-report"
 
-/bin/ln -i -s "$(/bin/pwd)/local/etc/cron.d/kbnet.com-zfs-report" "/etc/cron.d/zfs-report"
+/bin/ln -i -s "$(/bin/pwd)/local/etc/cron.d/kbcom.net-zfs-report" "/etc/cron.d/zfs-report"
